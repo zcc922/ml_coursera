0698985a01c3a1423c2b2d364363305146077d68
zdd996 <31197052+zdd996@users.noreply.github.com>
2017-08-21 12:19:50 +0800

    ex1 zdd

--- machine-learning-ex1/ex1/computeCost.m
+++ machine-learning-ex1/ex1/computeCost.m
@@ -13,7 +13,8 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
-
+res = X*theta - y; 
+J = (res' * res) / m*0.5; 
 
 % =========================================================================
 
--- machine-learning-ex1/ex1/gradientDescentMulti.m
+++ machine-learning-ex1/ex1/gradientDescentMulti.m
@@ -17,7 +17,8 @@ for iter = 1:num_iters
     %       of the cost function (computeCostMulti) and gradient here.
     %
 
-
+    t = ((X*theta - y)' *X); 
+    theta = theta - alpha /m * t; 
 
 
 
